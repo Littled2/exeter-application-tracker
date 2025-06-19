2c7b904127603fd636c126b5cda70a97acc4ff1b
Littled2 <edward.blewitt2@gmail.com>
2025-06-19 22:29:54 +0100

    Backend now closes if there is no .env file present

--- backend/pb_hooks/check_for_env.pb.js
+++ backend/pb_hooks/check_for_env.pb.js
@@ -1,14 +1,23 @@
 /// <reference path="../pb_data/types.d.ts" />
 
+/**
+ * Checks for the existence of a /env file on startup
+ * 
+ * If no file is found, throws an error and shuts down
+ */
+
 onBootstrap(e => {
 
     // Check that a .env file exists
     try {
 
-        let file = $os.readFile(__hooks + "/../.env")
+        let _ = $os.readFile(__hooks + "/../.env")
         
     } catch (error) {
-        console.log(`No .env file found, or error reading the .env file\n\nExpected .env file at: ${__hooks}/../.env\n\n`, error)
+        console.error(`\n*No .env file found*, or error reading the .env file\nExpected .env file at: ${__hooks}/../.env\n`)
+        console.error("Error message:", error)
+        console.log('Shutting down until the above is fixed')
+        $os.exit(2)
     }
 
     e.next()
