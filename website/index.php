6c1a2047b7562ff48b3ff3f2c8c07390c4b68e1f
Ed <edward.blewitt2@gmail.com>
2025-09-12 21:48:32 +0100

    Added rapid web analytics

--- website/index.php
+++ website/index.php
@@ -55,6 +55,7 @@
 
     <script type="module" src="/scripts/index.js"></script>
 
+    <script onload="attach_rapid_web_analytics('68abea027874c')" src="http://apps.edward-blewitt.uk/resources/web-analytics/analytics_v1.0.js"></script>
 
 </head>
 <body>
